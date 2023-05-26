24a792f32e0fe72162f45cfab53d1f6855d8656d
Gede <83480109+Gede18@users.noreply.github.com>
2023-05-26 14:33:39 -0500

    mi-archivo.js

--- mi-archivo.js
+++ mi-archivo.js
@@ -1,12 +1,22 @@
 function toggleVideo() {
-    var videoContainer = document.getElementById("video-container");
-    var video = document.getElementById("my-video");
-  
-    if (window.getComputedStyle(videoContainer).display === "none") {
-      videoContainer.style.display = "block";
-      video.play();
-    } else {
-      videoContainer.style.display = "none";
-      video.pause();
-    }
+  var videoContainer = document.getElementById("video-container");
+  var video = document.getElementById("my-video");
+
+  if (window.getComputedStyle(videoContainer).display === "none") {
+    videoContainer.style.display = "block";
+    video.play();
+  } else {
+    videoContainer.style.display = "none";
+    video.pause();
   }
+}
+$(document).ready(function() {
+  $('.carousel').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    adaptiveHeight: true
+  });
+});
+
